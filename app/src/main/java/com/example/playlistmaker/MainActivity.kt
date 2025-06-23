b688d034f43abc1cfbc459761d2bd05c5f34a1f8
roynys_pc <vadinload56@gmail.com>
2025-06-23 14:35:27 +0300

    дз2

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search_btn = findViewById<Button>(R.id.search)
+        val library_btn = findViewById<Button>(R.id.library)
+        val settings_btn = findViewById<Button>(R.id.settings)
+
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажал на поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        search_btn.setOnClickListener(searchClickListener)
+        library_btn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажал на медиатеку!", Toast.LENGTH_SHORT).show()
+        }
+        settings_btn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажал на настройки!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
